e0ab196b4c2b58cddd7e01ea752cdb427fba466d
coolwanglu <coolwanglu@0d3dcf9e-6d41-0410-9b3a-75061118c11e>
2010-01-03 15:45:25 +0000

    copyright stuff

--- trunk/target_memory_info_array.c
+++ trunk/target_memory_info_array.c
@@ -1,3 +1,23 @@
+/*
+ target_memory_info_array.c
+
+ Copyright (C) 2009 Eli Dupree  <elidupree(a)charter.net>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 
 #include <string.h>
 #include <stdbool.h>
