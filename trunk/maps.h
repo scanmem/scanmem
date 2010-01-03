bc204f4f7da69ec4a0cbdba7ddaa8247bbc22756
coolwanglu <coolwanglu@0d3dcf9e-6d41-0410-9b3a-75061118c11e>
2010-01-03 18:05:22 +0000

    Fix copyright format

--- trunk/maps.h
+++ trunk/maps.h
@@ -1,8 +1,7 @@
 /*
  $Id: maps.h,v 1.2 2007-06-05 01:45:35+01 taviso Exp $
  
- Copyright (C) 2009,2010      WANG  Lu      <coolwanglu(a)gmail.com>
- Copyright (C) 2009           Eli   Dupree  <elidupree(a)charter.net>
+ Copyright (C) 2009,2010      Tavis Ormandy, Eli Dupree, WANG Lu  <taviso@sdf.lonestar.org, elidupree@charter.net, coolwanglu@gmail.com>
  Copyright (C) 2006,2007,2009 Tavis Ormandy <taviso@sdf.lonestar.org>
  
  This program is free software; you can redistribute it and/or modify
