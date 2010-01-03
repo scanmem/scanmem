7a1112107ee178955cb76a28c7c31b0af6b810dd
coolwanglu <coolwanglu@0d3dcf9e-6d41-0410-9b3a-75061118c11e>
2010-01-03 13:59:17 +0000

    git-svn-id: https://scanmem.googlecode.com/svn/trunk@44 0d3dcf9e-6d41-0410-9b3a-75061118c11e

--- gui/consts.py
+++ gui/consts.py
@@ -5,5 +5,5 @@ VERSION = '0.09'
 DATA_DIR = '/usr/local/share/scanmem'
 
 SETTINGS = {'scan_data_type':'int32'
-           ,'search_scope'  : 2      # normal
+           ,'search_scope'  : 1      # normal
            }
