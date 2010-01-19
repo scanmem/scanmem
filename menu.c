7433e03751def27a8538f560f15dbedc0c3faa7a
coolwanglu@gmail.com <coolwanglu@gmail.com@0d3dcf9e-6d41-0410-9b3a-75061118c11e>
2010-01-19 12:50:56 +0000

    working on scan for double values on 32bit machines
    git-svn-id: https://scanmem.googlecode.com/svn/trunk@144 0d3dcf9e-6d41-0410-9b3a-75061118c11e

--- menu.c
+++ menu.c
@@ -54,7 +54,7 @@ bool getcommand(globals_t * vars, char **line)
 
     assert(vars != NULL);
 
-    snprintf(prompt, sizeof(prompt), "%ld>", vars->num_matches);
+    snprintf(prompt, sizeof(prompt), "%ld> ", vars->num_matches);
 
     rl_readline_name = "scanmem";
     rl_attempted_completion_function = commandcompletion;
