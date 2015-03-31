scanmem/scanmem

commit 944a54a01c3867753c315b1531534c033bf47fdf

Configurable endianness of data
This patch provides a runtime option, `endianness`. possible values:

0: data has same endianness as host
1: data is little endian
2: data is big endian

Commands that operate on words (set, write, and comparisons) respect
this setting when reading and writing.

Nothing is changed in the GUI, this is a command-line only option, for
now.

See https://code.google.com/p/scanmem/issues/detail?id=70

endianness.h
@@ -1,0 +1,36 @@
+/*
+ $Id: $
+
+ Copyright (C) 2014           Hraban Luyat
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef _ENDIANNESS_INC
+#define _ENDIANNESS_INC
+
+#include "scanmem.h"
+#include "value.h"
+
+static const union { short x; char y; } endiantest = { .x = 0xabcd };
+// True iff host is big endian
+static const bool big_endian = endiantest.y == 0xab;
+
+void fix_endianness(globals_t *vars, value_t *data_value);
+void swap_bytes_var(void *p, size_t num);
+
+#endif
+

Makefile.am
@@ -11,6 +11,7 @@ lib_LTLIBRARIES = libscanmem.la
 
 libscanmem_la_SOURCES = commands.h \
     commands.c \
+    endianness.c \
     ptrace.c \
     menu.c \
     handlers.h \

endianness.c
@@ -1,0 +1,91 @@
+/*
+ $Id: $
+ 
+ Copyright (C) 2014           Hraban Luyat
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "config.h"
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "endianness.h"
+#include "scanmem.h"
+#include "value.h"
+
+static uint16_t swap_bytes16(uint16_t i)
+{
+    uint16_t res = i & 0xff;
+    res <<= 8;
+    res |= i >> 8;
+    return res;
+}
+
+static uint32_t swap_bytes32(uint32_t i)
+{
+    uint32_t res = swap_bytes16(i);
+    res <<= 16;
+    res |= swap_bytes16(i >> 16);
+    return res;
+}
+
+static uint64_t swap_bytes64(uint64_t i)
+{
+    uint64_t res = swap_bytes32(i);
+    res <<= 32;
+    res |= swap_bytes32(i >> 32);
+    return res;
+}
+
+// swap endianness of 2, 4 or 8 byte word in-place.
+void swap_bytes_var(void *p, size_t num)
+{
+    switch (num) {
+    case sizeof(uint16_t): ; // empty statement to cheat the compiler
+        uint16_t i16 = swap_bytes16(*((uint16_t *)p));
+        memcpy(p, &i16, sizeof(uint16_t));
+        return;
+    case sizeof(uint32_t): ;
+        uint32_t i32 = swap_bytes32(*((uint32_t *)p));
+        memcpy(p, &i32, sizeof(uint32_t));
+        return;
+    case sizeof(uint64_t): ;
+        uint64_t i64 = swap_bytes64(*((uint64_t *)p));
+        memcpy(p, &i64, sizeof(uint64_t));
+        return;
+    }
+    assert(false);
+    return;
+}
+
+void fix_endianness(globals_t *vars, value_t *data_value)
+{
+    if (!vars->options.reverse_endianness) {
+        return;
+    }
+    if (data_value->flags.u64b) {
+        data_value->uint64_value = swap_bytes64(data_value->uint64_value);
+    } else if (data_value->flags.u32b) {
+        data_value->uint32_value = swap_bytes32(data_value->uint32_value);
+    } else if (data_value->flags.u16b) {
+        data_value->uint16_value = swap_bytes16(data_value->uint16_value);
+    }
+    return;
+}
+

handlers.c
@@ -47,6 +47,7 @@
 #include <readline/readline.h>
 
 #include "commands.h"
+#include "endianness.h"
 #include "handlers.h"
 #include "interrupt.h"
 #include "show_message.h"
@@ -254,6 +255,7 @@ bool handler__set(globals_t * vars, char **argv, unsigned argc)
                         show_info("setting *%p to %#"PRIx64"...\n", address, v.int64_value); 
 
                         /* set the value specified */
+                        fix_endianness(vars, &v);
                         if (setaddr(vars->target, address, &v) == false) {
                             show_error("failed to set a value.\n");
                             goto fail;
@@ -287,6 +289,7 @@ bool handler__set(globals_t * vars, char **argv, unsigned argc)
 
                         show_info("setting *%p to %"PRIx64"...\n", address, v.int64_value); 
 
+                        fix_endianness(vars, &v);
                         if (setaddr(vars->target, address, &v) == false) {
                             show_error("failed to set a value.\n");
                             goto fail;
@@ -1497,6 +1500,9 @@ bool handler__write(globals_t * vars, char **argv, unsigned argc)
             ret = false;
             goto retl;
         }
+        if (1 < data_width && vars->options.reverse_endianness) {
+            swap_bytes_var(buf, data_width);
+        }
         break;
     case 1: // bytearray
         ; /* cheat gcc */
@@ -1623,6 +1629,20 @@ bool handler__option(globals_t * vars, char **argv, unsigned argc)
         else
         {
             show_error("bad value for dump_with_ascii, see `help option`.\n");
+            return false;
+        }
+    }
+    else if (strcasecmp(argv[1], "endianness") == 0)
+    {
+        // data is host endian: don't swap
+        if (strcmp(argv[2], "0") == 0) {vars->options.reverse_endianness = 0; }
+        // data is little endian: swap if host is big endian
+        else if (strcmp(argv[2], "1") == 0) {vars->options.reverse_endianness = big_endian; }
+        // data is big endian: swap if host is little endian
+        else if (strcmp(argv[2], "2") == 0) {vars->options.reverse_endianness = !big_endian; }
+        else
+        {
+            show_error("bad value for endianness, see `help option`.\n");
             return false;
         }
     }

handlers.h
@@ -297,6 +297,14 @@ bool handler__write(globals_t * vars, char **argv, unsigned argc);
                  "\t0:\tdisabled\n" \
                  "\t1:\tenabled\n" \
                  "\n" \
+                 "endianness\tendianness of data (used by: set, write and comparisons)\n" \
+                 "\t\t\tDefault:0\n" \
+                 "\n" \
+                 "\tpossible values:\n" \
+                 "\t0:\thost endian\n" \
+                 "\t1:\tlittle endian\n" \
+                 "\t2:\tbig endian\n" \
+                 "\n" \
                  "Example:\n" \
                  "\toption scan_data_type int32\n"
 

ptrace.c
@@ -59,6 +59,7 @@
 #endif
 
 #include "value.h"
+#include "endianness.h"
 #include "scanroutines.h"
 #include "scanmem.h"
 #include "show_message.h"
@@ -314,6 +315,8 @@ bool checkmatches(globals_t * vars,
             truncval_to_flags(&old_val, flags);
             truncval_to_flags(&data_value, flags);
 
+            fix_endianness(vars, &data_value);
+
             memset(&checkflags, 0, sizeof(checkflags));
 
             match_length = (*g_scan_routine)(&data_value, &old_val, uservalue, &checkflags, address);
@@ -551,6 +554,8 @@ bool searchregions(globals_t * vars, scan_match_type_t match_type, const userval
                 break;
             }
 #endif
+
+            fix_endianness(vars, &data_value);
 
             memset(&checkflags, 0, sizeof(checkflags));
 

scanmem.h
@@ -73,6 +73,7 @@ typedef struct {
         region_scan_level_t region_scan_level;
         unsigned short detect_reverse_change;
         unsigned short dump_with_ascii;
+        unsigned short reverse_endianness;
     } options;
 } globals_t;
 

commit 3ee2eeab66cfabb4835dc03d6f5e395d3bd3d9aa

endianness: replace the broken big endian check with WORDS_BIGENDIAN
Commit 944a54a0 ("Configurable endianness of data") breaks the build
on some systems due to non-standard endianness detection. So check
if WORDS_BIGENDIAN is defined by AC_C_BIGENDIAN instead.

Tested on x86, x86_64 and in a Debian Wheezy ppc QEMU VM by adding
"#warning big endian detected" to the "big_endian = true" case.

Fixes: 944a54a01c38 ("Configurable endianness of data")

endianness.h
@@ -24,10 +24,14 @@
 
 #include "scanmem.h"
 #include "value.h"
+#include "config.h"
 
-static const union { short x; char y; } endiantest = { .x = 0xabcd };
-// True iff host is big endian
-static const bool big_endian = endiantest.y == 0xab;
+/* True if host is big endian */
+#ifdef WORDS_BIGENDIAN
+static const bool big_endian = true;
+#else
+static const bool big_endian = false;
+#endif
 
 void fix_endianness(globals_t *vars, value_t *data_value);
 void swap_bytes_var(void *p, size_t num);