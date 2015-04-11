6ff08ed19eeb1de38394aaf3260293cb6bba1004
Funmungus <funmungus@gmail.com>
2015-04-10 20:31:39 -0700

    Remove readline source, for legallity.
    Signed-off-by: Funmungus <funmungus@gmail.com>

--- libreplacements.h
+++ libreplacements.h
@@ -1,52 +1,34 @@
+/*
+    libreplacements.h  Minimal function replacements for libraries. 
+    Copyright (C) 2015 Funmungus  <funmungus(a)gmail.com>
 
-/* Copyright (C) 1991-2009 Free Software Foundation, Inc.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-   This file is part of the GNU Readline Library (Readline), a library
-   for reading lines of text with interactive input and history editing.      
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-   Readline is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   Readline is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with Readline.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #if !defined (LIBREPLACEMENTS_H)
 #define LIBREPLACEMENTS_H
 
-#define RL_SIG_RECEIVED() (_rl_caught_signal != 0)
-
-#if !defined (PARAMS)
-#  if defined (__STDC__) || defined (__GNUC__) || defined (__cplusplus)
-#    define PARAMS(protos) protos
-#  else
-#    define PARAMS(protos) ()
-#  endif
-#endif
-
-typedef char *rl_compentry_func_t PARAMS((const char *, int));
-typedef char **rl_completion_func_t PARAMS((const char *, int, int));
+typedef char *rl_compentry_func_t(const char *, int);
+typedef char **rl_completion_func_t(const char *, int, int);
 
 extern int rl_attempted_completion_over;
 extern const char *rl_readline_name;
 extern rl_completion_func_t *rl_attempted_completion_function;
 
-extern char **rl_completion_matches PARAMS((const char *, rl_compentry_func_t *));
-extern char *readline PARAMS((const char *));
-extern void add_history PARAMS((const char *));
-
-
-// Referenced functions
-extern void *xmalloc PARAMS((size_t));
-extern void *xrealloc PARAMS((void *, size_t));
-extern void xfree PARAMS((void *));
+extern char **rl_completion_matches(const char *, rl_compentry_func_t *);
+extern char *readline(const char *);
+extern void add_history(const char *);
 
 #endif
 
