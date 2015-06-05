scanmem/scanmem

commit 4e7e7c6bd102b303961d28d5102caaaae6244a32

gui: Update the scan progress only every 100 ms
The progress_watcher() routine is called via GObject.idle_add() and
is called too frequently on idle systems this way. So replace it
with GObject.timeout_add(), set the interval to 100 ms and set the
priority to GObject.PRIORITY_DEFAULT_IDLE again.

On idle systems this results in approximately 40% performance
improvement.

gui/GameConqueror.py
@@ -51,6 +51,7 @@ gettext.install(GETTEXT_PACKAGE, LOCALEDIR, names=('_'));
 
 CLIPBOARD = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
 WORK_DIR = os.path.dirname(sys.argv[0])
+PROGRESS_INTERVAL = 100 # for scan progress updates
 DATA_WORKER_INTERVAL = 500 # for read(update)/write(lock)
 SCAN_RESULT_LIST_LIMIT = 1000 # maximal number of entries that can be displayed
 
@@ -871,7 +872,8 @@ class GameConqueror():
         if self.search_count == 1:
             self.apply_scan_settings()
         self.backend.reset_scan_progress()
-        self.progress_watcher_id = GObject.idle_add(self.progress_watcher)
+        self.progress_watcher_id = GObject.timeout_add(PROGRESS_INTERVAL,
+            self.progress_watcher, priority=GObject.PRIORITY_DEFAULT_IDLE)
         threading.Thread(target=self.scan_thread_func, args=(cmd,)).start()
 
     def scan_thread_func(self, cmd):

commit 65c0bdeb9ee50f0bb766a868dffb4d09783da4fa

gui: Set the progress to 100% after scanning
Commit 4e7e7c6b ("gui: Update the scan progress only every 100 ms")
causes the scan progress bar not to be completely filled after
scanning. So set it to 100% explicitly then.

Fixes: 4e7e7c6bd102 ("gui: Update the scan progress only every 100 ms")

gui/GameConqueror.py
@@ -885,6 +885,7 @@ class GameConqueror():
         GObject.source_remove(self.progress_watcher_id)
         Gdk.threads_enter()
 
+        self.scanprogress_progressbar.set_fraction(1.0)
         self.main_window.set_sensitive(True)
         self.memoryeditor_window.set_sensitive(True)
         self.is_scanning = False