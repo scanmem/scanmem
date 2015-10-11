scanmem/scanmem

commit e87de1b70d24d128049a09f2b2429be7a40a0c6c

clean up config.h , headers, and printf formats
git-svn-id: https://scanmem.googlecode.com/svn/trunk@141 0d3dcf9e-6d41-0410-9b3a-75061118c11e

handlers.c
@@ -24,6 +24,8 @@
 # define _GNU_SOURCE
 #endif
 
+#include "config.h"
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -40,13 +42,14 @@
 #include <sys/time.h>
 #include <limits.h>            /* to determine the word width */
 #include <errno.h>
+#include <inttypes.h>
 
 #include <readline/readline.h>
 
-#include "scanmem.h"
 #include "commands.h"
 #include "handlers.h"
 #include "interrupt.h"
+#include "show_message.h"
 
 #define USEPARAMS() ((void) vars, (void) argv, (void) argc)     /* macro to hide gcc unused warnings */
 
@@ -228,7 +231,7 @@ bool handler__set(globals_t * vars, char **argv, unsigned argc)
                         value_t old;
                         void *address = remote_address_of_nth_element(loc.swath, loc.index /* ,MATCHES_AND_VALUES */);
 
-                        show_info("setting *%p to %#llx...\n", address, (long long)userval.int64_value); 
+                        show_info("setting *%p to %#"PRIx64"...\n", address, userval.int64_value); 
                         
                         /* copy val onto v */
                         /* XXX: valcmp? make sure the sizes match */
@@ -269,7 +272,7 @@ bool handler__set(globals_t * vars, char **argv, unsigned argc)
                         v.flags = old.flags;
                         uservalue2value(&v, &userval);
 
-                        show_info("setting *%p to %#llx...\n", address, (long long)v.int64_value); 
+                        show_info("setting *%p to %"PRIx64"...\n", address, v.int64_value); 
 
                         if (setaddr(vars->target, address, &v) == false) {
                             show_error("failed to set a value.\n");
@@ -1314,28 +1317,28 @@ bool handler__write(globals_t * vars, char **argv, unsigned argc)
     {
         data_width = 1;
         datatype = 0;
-        fmt = "%hhd";
+        fmt = "%"PRId8;
     }
     else if ((strcasecmp(argv[1], "i16") == 0)
            ||(strcasecmp(argv[1], "int16") == 0))
     {
         data_width = 2;
         datatype = 0;
-        fmt = "%hd";
+        fmt = "%"PRId16;
     }
     else if ((strcasecmp(argv[1], "i32") == 0)
            ||(strcasecmp(argv[1], "int32") == 0))
     {
         data_width = 4;
         datatype = 0;
-        fmt = "%d";
+        fmt = "%"PRId32;
     }
     else if ((strcasecmp(argv[1], "i64") == 0)
            ||(strcasecmp(argv[1], "int64") == 0))
     {
         data_width = 8;
         datatype = 0;
-        fmt = "%lld";
+        fmt = "%"PRId64;
     }
     else if ((strcasecmp(argv[1], "f32") == 0)
            ||(strcasecmp(argv[1], "float32") == 0))  

commands.c
@@ -24,6 +24,8 @@
 # define _GNU_SOURCE
 #endif
 
+#include "config.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
@@ -35,8 +37,8 @@
 
 #include <readline/readline.h>
 
-#include "scanmem.h"
 #include "commands.h"
+#include "show_message.h"
 
 /*
  * registercommand - add the command and a pointer to its handler to the commands list.

commands.h
@@ -5,6 +5,10 @@
 #ifndef _COMMANDS_INC
 #define _COMMANDS_INC           /* include guard */
 
+#include <stdbool.h>
+
+#include "scanmem.h"
+
 /*lint -esym(534,registercommand) okay to ignore return value */
 
 typedef struct {

list.c
@@ -20,6 +20,8 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#include "config.h"
+
 #include <stdlib.h>
 
 #include "list.h"

main.c
@@ -24,6 +24,8 @@
 # define _GNU_SOURCE
 #endif
 
+#include "config.h"
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <stdlib.h>
@@ -34,6 +36,7 @@
 #include "scanmem.h"
 #include "commands.h"
 #include "handlers.h"
+#include "show_message.h"
 
 static void printhelp(void);
 static void sighandler(int n);

maps.c
@@ -24,6 +24,8 @@
 # define _GNU_SOURCE
 #endif
 
+#include "config.h"
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <stddef.h>
@@ -36,6 +38,7 @@
 #include "list.h"
 #include "maps.h"
 #include "scanmem.h"
+#include "show_message.h"
 
 bool readmaps(pid_t target, list_t * regions)
 {

menu.c
@@ -22,6 +22,8 @@
 # define _GNU_SOURCE
 #endif
 
+#include "config.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -34,6 +36,7 @@
 
 #include "scanmem.h"
 #include "commands.h"
+#include "show_message.h"
 
 static char *commandgenerator(const char *text, int state);
 static char **commandcompletion(const char *text, int start, int end);

ptrace.c
@@ -20,15 +20,7 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-/* prepare LARGEFILE support, i'll autoconf this later */
-# ifdef _FILE_OFFSET_BITS
-#  undef _FILE_OFFSET_BITS
-# endif
-# define _FILE_OFFSET_BITS 64
-# ifdef _LARGEFILE64_SOURCE
-#  undef _LARGEFILE64_SOURCE
-# endif
-# define _LARGEFILE64_SOURCE
+#include "config.h"
 
 /* for pread */
 # ifdef _XOPEN_SOURCE
@@ -61,6 +53,7 @@
 #include "value.h"
 #include "scanroutines.h"
 #include "scanmem.h"
+#include "show_message.h"
 
 #define min(a,b) (((a)<(b))?(a):(b))
 

commit ba6eef5ef197c5b9fa300c22fed596a551ab11b0

handlers: set: Always print set values with '0x' prefix
The commit e87de1b70d24 ("clean up config.h , headers, and printf
formats") causes since 2010 that the printed output of setting all
matches at once and setting them separately is different. In the
case that all matches are set at once, the '0x' prefix is missing
for the set values. It was there before. So add it back.

Reference: GitHub issue #134
Fixes: e87de1b70d24 ("clean up config.h , headers, and printf formats")

handlers.c
@@ -251,7 +251,7 @@ bool handler__set(globals_t * vars, char **argv, unsigned argc)
                         v.flags = old.flags = loc.swath->data[loc.index].match_info;
                         uservalue2value(&v, &userval);
                         
-                        show_info("setting *%p to %#"PRIx64"...\n", address, v.int64_value); 
+                        show_info("setting *%p to %#"PRIx64"...\n", address, v.int64_value);
 
                         /* set the value specified */
                         fix_endianness(vars, &v);
@@ -287,7 +287,7 @@ bool handler__set(globals_t * vars, char **argv, unsigned argc)
                         v.flags = old.flags = reading_swath_index->data[reading_iterator].match_info;
                         uservalue2value(&v, &userval);
 
-                        show_info("setting *%p to %"PRIx64"...\n", address, v.int64_value); 
+                        show_info("setting *%p to %#"PRIx64"...\n", address, v.int64_value);
 
                         fix_endianness(vars, &v);
                         if (setaddr(vars->target, address, &v) == false) {