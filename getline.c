6a6f22a89d9a8723b042bcaa675e1653123c0653
Jonathan Pelletier <funmungus@gmail.com>
2015-10-13 17:15:54 -0700

    getline: Introduce a getline() replacement
    Android systems not only have no readline() support but they also
    have no getline() support. What they have is the BSD specific
    fgetln() function. So provide a getline() replacement function
    based on fgetln() which provides the same functionality.
    
    The difference between these functions is that fgetln() does no
    memory allocations and it returns characters without the null
    byte ('\0') termination. The minimum allocation is 120 bytes.
    
    This is based on the work of JianXiong Zhou.
    
    [@sriemer: major rework, wrote the commit description]

---
+++ getline.c
@@ -0,0 +1,85 @@
+/*
+    Replace getline() with BSD specific fgetln().
+
+    Copyright (C) 2015 JianXiong Zhou <zhoujianxiong2@gmail.com>
+    Copyright (C) 2015 Jonathan Pelletier <funmungus(a)gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "getline.h"
+
+#if !defined(HAVE_GETLINE) && defined(HAVE_FGETLN)
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+/*
+ * Avoid frequent malloc()/free() calls
+ * (determined by getline() test on Linux)
+ */
+#define BUF_MIN 120
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream)
+{
+    char *lptr;
+    size_t len = 0;
+
+    /* Check for invalid arguments */
+    if (lineptr == NULL || n == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    lptr = fgetln(stream, &len);
+    if (lptr == NULL) {
+        /* Invalid stream */
+        errno = EINVAL;
+        return -1;
+    }
+
+    /*
+     * getline() returns a null byte ('\0') terminated C string
+     * but fgetln() returns characters without '\0' termination
+     */
+    if (*lineptr == NULL) {
+        *n = BUF_MIN;
+        goto alloc_buf;
+    }
+
+    /* Realloc the original pointer */
+    if (*n < len + 1) {
+        free(*lineptr);
+
+        *n = len + 1;
+alloc_buf:
+        *lineptr = malloc(*n);
+        if (*lineptr == NULL) {
+            *n = 0;
+            return -1;
+        }
+    }
+
+    /* Copy over the string */
+    memcpy(*lineptr, lptr, len);
+    (*lineptr)[len] = '\0';
+
+    /*
+     * getline() and fgetln() both return len including the
+     * delimiter but without the null byte at the end
+     */
+    return len;
+}
+
+#endif
---
+++ getline.h
@@ -0,0 +1,32 @@
+/*
+    Replace getline() with BSD specific fgetln().
+
+    Copyright (C) 2015 JianXiong Zhou <zhoujianxiong2@gmail.com>
+    Copyright (C) 2015 Jonathan Pelletier <funmungus(a)gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GETLINE_H
+#define GETLINE_H
+
+#include "config.h"
+
+#ifndef HAVE_GETLINE
+#include <stdio.h>
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+#endif
+
+#endif /* GETLINE_H */
