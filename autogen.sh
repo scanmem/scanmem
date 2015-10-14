383c9821ec9eddc9c83f9c5730411b29159b6b36
Jonathan Pelletier <funmungus@gmail.com>
2015-10-13 17:18:28 -0700

    autogen.sh: Add Darwin support
    On Darwin systems there is no libtoolize. But glibtoolize is
    available via brew and does the same. So detect Darwin and use
    glibtoolize there.
    
    [@sriemer: wrote the commit description]

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-libtoolize -c && aclocal -I m4 && automake -c --add-missing && autoconf && intltoolize -f -c
+
+LIBTOOLIZE=libtoolize
+if [ "$(uname -s)" = "Darwin" ]; then
+    LIBTOOLIZE=glibtoolize  # install via brew
+fi
+
+$LIBTOOLIZE -c && aclocal -I m4 && automake -c --add-missing && autoconf && intltoolize -f -c
