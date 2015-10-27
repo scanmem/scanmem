scanmem/scanmem

commit e1f179d9a3277ccb73d5c6480cd104698fdeefd7

fix enable/with arguments in configure.ac; renames libreplacements* to readline*

menu.c
@@ -32,11 +32,11 @@
 #include <string.h>
 #include <stdbool.h>
 
-#if defined(HAVE_LIBREADLINE) && HAVE_LIBREADLINE
+#if WITH_READLINE
 #include <readline/readline.h>
 #include <readline/history.h>
 #else
-#include "libreplacements.h"
+#include "readline.h"
 #endif
 
 #include "scanmem.h"

Makefile.am
@@ -33,9 +33,9 @@ libscanmem_la_SOURCES = commands.h \
     value.h \
     value.c 
 
-if ENABLE_LIBLESS
-  libscanmem_la_SOURCES += libreplacements.h \
-      libreplacements.c
+if !WITH_READLINE
+  libscanmem_la_SOURCES += readline.h \
+      readline.c
 endif
 
 libscanmem_la_LDFLAGS = -version-info 1:0:0

README
@@ -30,13 +30,10 @@ To build without gui:
     ./configure --prefix=/usr && make
     sudo make install
 
-To build without gui and without library features:
-
-    ./configure --prefix=/usr --enable-libless && make
-    sudo make install
-
 If you want to specify a prefix other than '/usr', make sure that <prefix>/lib is in the
 LD_LIBRARY_PATH for root
+
+Run `./configure --help` for more details.
 
 ## GUI
 

configure.ac
@@ -112,39 +112,30 @@ AC_CONFIG_FILES([
                 ])
 
 AC_ARG_ENABLE(gui, [AS_HELP_STRING([--enable-gui],
-                            [enable gameconqueror, the gui front-end of scanmem])],
-                 [enable_gui=true
-                  AC_CONFIG_FILES([
-                 gui/Makefile
-                 gui/icons/Makefile
-                 gui/consts.py
-                 gui/gameconqueror
-                 gui/org.freedesktop.gameconqueror.policy.in
-                 ])],
-                 [enable_gui=false]
-                 )
+                            [enable gameconqueror, the gui front-end of scanmem])])
+AM_CONDITIONAL([ENABLE_GUI], [test "x$enable_gui" = "xyes"])
+AS_IF([test "x$enable_gui" = "xyes"], [
+  AC_CONFIG_FILES([
+    gui/Makefile
+    gui/icons/Makefile
+    gui/consts.py
+    gui/gameconqueror
+    gui/org.freedesktop.gameconqueror.policy.in
+  ])
+])
 
 # Check for termcap and readline or bypass checking for the libraries.
-AC_ARG_ENABLE([libless], [AS_HELP_STRING([--enable-libless],
-                            [use a libreadline replacement with less features])],
-                 [enable_libless=true
-                 echo "Bypassing library linking (--enable-libless)."
-                 AC_DEFINE(HAVE_LIBTERMCAP, [0],
-                     [Define to 1 if you have the 'termcap' library (-ltermcap)])
-                 AC_DEFINE(HAVE_LIBREADLINE, [0],
-                     [Define to 1 if you have the 'readline' library (-lreadline)])
-                 ],
-                 [enable_libless=false
-                 echo "Using libraries if available."
-                 # termcap is sometimes required by readline
-                 AC_CHECK_LIB([termcap], [tgetent], [], [])
-                 AC_CHECK_LIB([readline], [readline], [], [
-                     echo "libreadline could not be found, which is required to continue."
-                     echo "The libreadline-dev package may be required."
-                     exit 1
-                     ])
-                 ])
+AC_ARG_WITH([readline], [AS_HELP_STRING([--without-readline],
+                            [build without readline])])
+AM_CONDITIONAL([WITH_READLINE], [test "x$with_readline" != "xno"])
+AS_IF([test "x$with_readline" != "xno"], [
+  # termcap is sometimes required by readline
+  AC_CHECK_LIB([termcap], [tgetent], [], [])
+  AC_CHECK_LIB([readline], [readline], [], [
+    echo "libreadline could not be found, which is required to continue."
+    echo "The libreadline-dev package may be required."
+    exit 1
+  ])
+])
 
-AM_CONDITIONAL([ENABLE_GUI], [test x$enable_gui = xtrue])
-AM_CONDITIONAL([ENABLE_LIBLESS], [test x$enable_libless = xtrue])
 AC_OUTPUT

readline.c
@@ -1,5 +1,5 @@
 /*
- libreplacements.c  Minimal function replacements for libraries.
+ readline.c: replaces libreadline
 
  Copyright (C) 2015 Jonathan Pelletier <funmungus(a)gmail.com>
 
@@ -20,7 +20,7 @@
 
 #include <stdio.h>
 
-#include "libreplacements.h"
+#include "readline.h"
 
 int rl_attempted_completion_over = 0;
 const char *rl_readline_name = "scanmem";

readline.h
@@ -1,5 +1,5 @@
 /*
- libreplacements.h  Minimal function replacements for libraries.
+ readline.h: replaces libreadline
 
  Copyright (C) 2015 Jonathan Pelletier <funmungus(a)gmail.com>
 
@@ -18,8 +18,8 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef _LIBREPLACEMENTS_INC
-#define _LIBREPLACEMENTS_INC
+#ifndef _READLINE_INC
+#define _READLINE_INC
 
 typedef char *rl_compentry_func_t(const char *, int);
 typedef char **rl_completion_func_t(const char *, int, int);
@@ -33,4 +33,4 @@ char **rl_completion_matches(const char *text, rl_compentry_func_t
 char *readline(const char *prompt);
 void add_history(const char *line);
 
-#endif
+#endif /* _READLINE_INC */

commit 64d5e762bc5f9d970c08d04bcbd222b605ce8941

menu: Check the correct libreadline define
WITH_READLINE is defined for make files and HAVE_LIBREADLINE is
defined in config.h. Checking for WITH_READLINE is not correct here.
So check for HAVE_LIBREADLINE instead.

Fixes: e1f179d9a327 ("fix enable/with arguments in configure.ac...")

menu.c
@@ -31,7 +31,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-#if WITH_READLINE
+#if HAVE_LIBREADLINE
 #include <readline/readline.h>
 #include <readline/history.h>
 #else