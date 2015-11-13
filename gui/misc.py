scanmem/scanmem

commit 4abe7b3178938a8971fb23870eb230d037b859bc

gui: Remove isinstance() calls for classinfo 'long'
Class 'long' is avoided in Python 2 and has been removed in Python 3.
Long values fit into class 'int' and only isinstance() calls use
class 'long' in GameConqueror. With Python 3, entering number values
with a decimal point (e.g. "1.0") results in the 'Bad value' error
message as check_scan_command() checks for a 'long' instance before
it checks for a 'float' instance. So remove all checks for 'long'
instances.

Reported-by: @calculuswhiz
Fixes: GitHub issue #140

gui/misc.py
@@ -77,7 +77,7 @@ def check_scan_command (data_type, cmd, is_first_scan):
         cmd += str(num)
 
         if data_type.startswith('int'):
-            if not (isinstance(num, int) or isinstance(num, long)):
+            if not (isinstance(num, int)):
                 raise ValueError(_('%s is not an integer') % (num,))
             if data_type == 'int':
                 width = 64
@@ -93,7 +93,7 @@ def check_scan_command (data_type, cmd, is_first_scan):
 def eval_operand(s):
     try:
         v = eval(s)
-        if isinstance(v, int) or isinstance(v, long) or isinstance(v, float):
+        if isinstance(v, int) or isinstance(v, float):
             return v
     except:
         pass

commit 1a2e9bd99dc70a85481509ead3bc59147dec8c21

gui: Py2: Check again for 'long' instances
The commit 4abe7b317893 ("gui: Remove isinstance() calls for
classinfo 'long'") fixes the Python 3 issue but breaks GameConqueror
with Python 2 on 32-bit i686 systems. So check the Python version
first and only call isinstance() for classinfo 'long' if Python < 3
is detected.

This has been tested with Python 2.7 on i686 and 2.7 as well as 3.4
on x86_64.

Reference: GitHub issue #140
Fixes: 4abe7b317893 ("gui: Remove isinstance() calls for class long")

gui/misc.py
@@ -77,7 +77,8 @@ def check_scan_command (data_type, cmd, is_first_scan):
         cmd += str(num)
 
         if data_type.startswith('int'):
-            if not (isinstance(num, int)):
+            py2_long = not PY3K and isinstance(num, long)
+            if not (isinstance(num, int) or py2_long):
                 raise ValueError(_('%s is not an integer') % (num,))
             if data_type == 'int':
                 width = 64
@@ -93,7 +94,8 @@ def check_scan_command (data_type, cmd, is_first_scan):
 def eval_operand(s):
     try:
         v = eval(s)
-        if isinstance(v, int) or isinstance(v, float):
+        py2_long = not PY3K and isinstance(v, long)
+        if isinstance(v, int) or isinstance(v, float) or py2_long:
             return v
     except:
         pass