scanmem/scanmem

commit aedf0695b899a9bc0afc83cacd14411fa019ccc3

gui: add match offset and region type support
The format of the scanmem 'list' command has changed. So the GUI
has to be changed as well. The match offset from the code load
address or region start as well as the region type are also
useful in the ScanResult_TreeView. So add them to new columns.

gui/GameConqueror.py
@@ -157,13 +157,16 @@ class GameConqueror():
         # we may need a cell data func here
         # create model
         self.scanresult_tv = self.builder.get_object('ScanResult_TreeView')
-        self.scanresult_liststore = Gtk.ListStore(str, str, str, bool) #addr, value, type, valid
+        # liststore contents: addr, value, type, valid, offset, region type
+        self.scanresult_liststore = Gtk.ListStore(str, str, str, bool, str, str)
         self.scanresult_tv.set_model(self.scanresult_liststore)
         self.scanresult_tv.get_selection().set_mode(Gtk.SelectionMode.MULTIPLE)
         self.scanresult_last_clicked = 0
         # init columns
         misc.treeview_append_column(self.scanresult_tv, _('Address'), attributes=(('text',0),), properties = (('family', 'monospace'),))
         misc.treeview_append_column(self.scanresult_tv, _('Value'), attributes=(('text',1),), properties = (('family', 'monospace'),))
+        misc.treeview_append_column(self.scanresult_tv, _('Offset'), attributes=(('text',4),), properties = (('family', 'monospace'),))
+        misc.treeview_append_column(self.scanresult_tv, _('Region Type'), attributes=(('text',5),), properties = (('family', 'monospace'),))
 
         # init CheatList TreeView
         self.cheatlist_tv = self.builder.get_object('CheatList_TreeView')
@@ -903,10 +906,11 @@ class GameConqueror():
             self.scanresult_liststore.clear()
             for line in lines:
                 line = line[line.find(']')+1:]
-                (a, v, t) = list(map(str.strip, line.split(',')[:3]))
+                (a, o, rt, v, t) = list(map(str.strip, line.split(',')[:5]))
                 a = '%x'%(int(a,16),)
+                o = '%x'%(int(o[5:],16),)
                 t = t[1:-1]
-                self.scanresult_liststore.append([a, v, t, True])
+                self.scanresult_liststore.append([a, v, t, True, o, rt])
             self.scanresult_tv.set_model(self.scanresult_liststore)
 
     # return range(r1, r2) where all rows between r1 and r2 (EXCLUSIVE) are visible

commit 454977c9180c2657313badea0b19c99c67b061e8

gui: Fix a crash with matches and region ID > 99
In the rare event of finding matches in regions with a region ID
> 99, GameConqueror crashes. The debug output points to the line
which splits the match offset from the region ID of the match.
Reserving two chars for the region ID is plain not enough and
relying on its length causes trouble. So split the match offset
from the region ID by searching for the '+' character.

This has been tested with Python 2.7 and 3.4.

Fixes: GitHub issue #142
Fixes: aedf0695b899 ("gui: add match offset and region type support")
Reported-by: @calculuswhiz

gui/GameConqueror.py
@@ -915,7 +915,7 @@ class GameConqueror():
                 line = line[line.find(']')+1:]
                 (a, o, rt, v, t) = list(map(str.strip, line.split(',')[:5]))
                 a = '%x'%(int(a,16),)
-                o = '%x'%(int(o[5:],16),)
+                o = '%x'%(int(o.split('+')[1],16),)
                 t = t[1:-1]
                 if t == 'unknown':
                     continue