a83e21f552dc8ff20d15662e105c8a452ef9c354
Andrea Stacchiotti <crudepatate@gmail.com>
2016-05-31 21:03:00 +0200

    gui/GameConqueror.py: Fix typo

--- gui/GameConqueror.py
+++ gui/GameConqueror.py
@@ -725,7 +725,7 @@ class GameConqueror():
         try:
             self.read_maps()
         except:
-            self.show_error(_('Cannot retieve memory maps of that process, maybe it has exited (crashed), or you don\'t have enough privilege'))
+            self.show_error(_('Cannot retrieve memory maps of that process, maybe it has exited (crashed), or you don\'t have enough privilege'))
             return
         selected_region = None
         if addr:
@@ -807,7 +807,7 @@ class GameConqueror():
             self.pid = 0
             self.process_label.set_text(_('No process selected'))
             self.process_label.set_property('tooltip-text', _('Select a process'))
-            self.show_error(_('Cannot retieve memory maps of that process, maybe it has exited (crashed), or you don\'t have enough privilege'))
+            self.show_error(_('Cannot retrieve memory maps of that process, maybe it has exited (crashed), or you don\'t have enough privilege'))
         self.process_label.set_text('%d - %s' % (pid, process_name))
         self.process_label.set_property('tooltip-text', process_name)
 
