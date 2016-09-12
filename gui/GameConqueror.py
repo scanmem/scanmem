scanmem/scanmem

commit 8efffac315f2efcd69fd3290387d9f7d627a2e4a

gui: Change the ComboBox's to ComboBoxText's
As we only use them for text, this makes working with them a lot
easier.

gui/GameConqueror.py
@@ -58,11 +58,11 @@ PROGRESS_INTERVAL = 100 # for scan progress updates
 DATA_WORKER_INTERVAL = 500 # for read(update)/write(lock)
 SCAN_RESULT_LIST_LIMIT = 1000 # maximal number of entries that can be displayed
 
-SCAN_VALUE_TYPES = misc.build_simple_str_liststore(['int', 'int8', 'int16', 'int32', 'int64', 'float', 'float32', 'float64', 'number', 'bytearray', 'string' ])
+SCAN_VALUE_TYPES = ['int', 'int8', 'int16', 'int32', 'int64', 'float', 'float32', 'float64', 'number', 'bytearray', 'string']
 
 LOCK_FLAG_TYPES = misc.build_simple_str_liststore(['=', '+', '-'])
 
-LOCK_VALUE_TYPES = misc.build_simple_str_liststore(['int8', 'int16', 'int32', 'int64', 'float32', 'float64', 'bytearray', 'string' ])
+LOCK_VALUE_TYPES = ['int8', 'int16', 'int32', 'int64', 'float32', 'float64', 'bytearray', 'string']
 
 SEARCH_SCOPE_NAMES = ['Basic', 'Normal', 'Full']
 
@@ -142,8 +142,9 @@ class GameConqueror():
 
         ###
         # Set scan data type
-        self.scan_data_type_combobox = self.builder.get_object('ScanDataType_ComboBox')
-        misc.build_combobox(self.scan_data_type_combobox, SCAN_VALUE_TYPES)
+        self.scan_data_type_combobox = self.builder.get_object('ScanDataType_ComboBoxText')
+        for entry in SCAN_VALUE_TYPES:
+            self.scan_data_type_combobox.append_text(entry)
         # apply setting
         misc.combobox_set_active_item(self.scan_data_type_combobox, SETTINGS['scan_data_type'])
 
@@ -236,7 +237,7 @@ class GameConqueror():
                                         ,attributes = (('text',4),)
                                         ,properties = (('editable', True)
                                                       ,('has-entry', False)
-                                                      ,('model', LOCK_VALUE_TYPES)
+                                                      ,('model', misc.build_simple_str_liststore(LOCK_VALUE_TYPES))
                                                       ,('text-column', 0))
                                         ,signals = (('edited', self.cheatlist_edit_type_cb),
                                                     ('editing-started', self.cheatlist_edit_start),
@@ -292,8 +293,9 @@ class GameConqueror():
         # init AddCheatDialog
         self.addcheat_address_input = self.builder.get_object('Address_Input')
         self.addcheat_description_input = self.builder.get_object('Description_Input')
-        self.addcheat_type_combobox = self.builder.get_object('Type_ComboBox')
-        misc.build_combobox(self.addcheat_type_combobox, LOCK_VALUE_TYPES)
+        self.addcheat_type_combobox = self.builder.get_object('Type_ComboBoxText')
+        for entry in LOCK_VALUE_TYPES:
+            self.addcheat_type_combobox.append_text(entry)
         misc.combobox_set_active_item(self.addcheat_type_combobox, SETTINGS['lock_data_type'])
         self.addcheat_dialog.connect('delete-event', lambda acd, e: acd.hide() or True)
         
@@ -921,9 +923,8 @@ class GameConqueror():
 
     def apply_scan_settings (self):
         # scan data type
-        active = self.scan_data_type_combobox.get_active()
-        assert(active >= 0)
-        dt = self.scan_data_type_combobox.get_model()[active][0]
+        assert(self.scan_data_type_combobox.get_active() >= 0)
+        dt = self.scan_data_type_combobox.get_active_text()
 
         self.command_lock.acquire()
         self.backend.send_command('option scan_data_type %s' % (dt,))
@@ -940,9 +941,8 @@ class GameConqueror():
         if self.pid == 0:
             self.show_error(_('Please select a process'))
             return
-        active = self.scan_data_type_combobox.get_active()
-        assert(active >= 0)
-        data_type = self.scan_data_type_combobox.get_model()[active][0]
+        assert(self.scan_data_type_combobox.get_active() >= 0)
+        data_type = self.scan_data_type_combobox.get_active_text()
         cmd = self.value_input.get_text()
    
         try:

gui/GameConqueror.xml
@@ -441,7 +441,7 @@ along with GameConqueror.  If not, see <http://www.gnu.org/licenses/>.
                             <property name="can_focus">False</property>
                             <property name="column_spacing">5</property>
                             <child>
-                              <object class="GtkComboBox" id="ScanDataType_ComboBox">
+                              <object class="GtkComboBoxText" id="ScanDataType_ComboBoxText">
                                 <property name="visible">True</property>
                                 <property name="can_focus">False</property>
                               </object>
@@ -831,7 +831,7 @@ Special thanks: Igor Gnatenko, Andrea Stacchiotti, Mattias Muenster, Zhang Zhe</
               </packing>
             </child>
             <child>
-              <object class="GtkComboBox" id="Type_ComboBox">
+              <object class="GtkComboBoxText" id="Type_ComboBoxText">
                 <property name="width_request">12</property>
                 <property name="visible">True</property>
                 <property name="can_focus">False</property>

gui/misc.py
@@ -127,13 +127,6 @@ def build_simple_str_liststore(l):
         r.append([e])
     return r
 
-# create a renderer for the combobox, set the text to `col`
-def build_combobox(combobox, model, col=0):
-    combobox.set_model(model)
-    renderer = Gtk.CellRendererText()
-    combobox.pack_start(renderer, True)
-    combobox.add_attribute(renderer, 'text', col)
-
 # set active item of the `combobox`
 # such that the value at `col` is `name`
 def combobox_set_active_item(combobox, name, col=0):

commit 07dd37deb056da33873b59ff2d8596f5dcc7ee27

gui: Fix "Manually add cheat" regression
After changing the ComboBox's in ComboBoxText's one of the calls
to retrieve the content wasn't updated to the new API.
So do it now.

Fixes: 8efffac315f2 ("gui: Change the ComboBox's to ComboBoxText's")
Reported-by: @sriemer
Reference: #212

gui/GameConqueror.py
@@ -382,7 +382,7 @@ class GameConqueror():
 
         description = self.addcheat_description_input.get_text()
         if not description: description = _('No Description')
-        typestr = LOCK_VALUE_TYPES[self.addcheat_type_combobox.get_active()][0]
+        typestr = self.addcheat_type_combobox.get_active_text()
         if 'int' in typestr: value = 0
         elif 'float' in typestr: value = 0.0
         elif 'string' in typestr: value = ''