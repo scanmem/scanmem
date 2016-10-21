scanmem/scanmem

commit 858acabb1975c4522cd2ef9ec07d6644d083438c

gui: convert payload data for hexview to unicode
With Python 3 the hexview doesn't open and that crashes the GUI.
The payload data read from memory is stored in bytes instead of
unicode. So it has to be converted but converting to utf-8 with
decode() doesn't work here.
With introduction of the function u() it is possible to convert to
unicode just like Python 2 does. Hence use it here.

Fixes: GitHub issue #103

gui/GameConqueror.py
@@ -39,6 +39,7 @@ from gi.repository import GObject
 from consts import *
 from hexview import HexView
 from backend import GameConquerorBackend
+from misc import u
 import misc
 
 import locale
@@ -741,7 +742,7 @@ class GameConqueror():
                 self.show_error(_('Cannot read memory'))
                 return
             self.last_hexedit_address = (start_addr, end_addr)
-            self.memoryeditor_hexview.payload=data
+            self.memoryeditor_hexview.payload = u(data)
             self.memoryeditor_hexview.base_addr = start_addr
         
         # set editable flag

commit 76a294c2deafedcb2a80b671e4bd3667484c16a9

gui: hexview: Fix unicode conversion for Python 3
The function u() has trouble decoding certain escape sequences
like '\\n'. The string '\n' is not converted to the byte sequence
5C 6E but 0A instead. This also causes a traceback due to the
shorter length.

Python 2 and 3 are both able to transform raw memory into an
integer array and to successfully process it. So introduce and use
the str2bytes() function for that.

Reported-by: Sebastian Parschauer (@sriemer)
Fixes: 858acabb1975 ("gui: convert payload data for hexview to unicode")
Fixes #220

gui/GameConqueror.py
@@ -846,7 +846,7 @@ class GameConqueror():
                 self.show_error(_('Cannot read memory'))
                 return
             self.last_hexedit_address = (start_addr, end_addr)
-            self.memoryeditor_hexview.payload = misc.u(data)
+            self.memoryeditor_hexview.payload = misc.str2bytes(data)
             self.memoryeditor_hexview.base_addr = start_addr
         
         # set editable flag

gui/hexview.py
@@ -209,7 +209,7 @@ class AsciiText(BaseText):
         output = []
 
         convert = lambda i: "".join(
-            [(x in AsciiText._printable) and (x) or ('.') for x in list(i)])
+            [chr(x) if (chr(x) in AsciiText._printable) else ('.') for x in list(i)])
 
         for i in range(tot_lines):
             if i * bpl + bpl > len(txt):
@@ -413,7 +413,7 @@ class HexText(BaseText):
             tot_lines += 1
 
         output = []
-        convert = lambda x: '%02X'%(ord(x),)
+        convert = lambda x: '%02X'%(x,)
 
         for i in range(tot_lines):
             if i * bpl + bpl > len(txt):

gui/misc.py
@@ -3,6 +3,7 @@
     
     Copyright (C) 2010,2011,2013 Wang Lu <coolwanglu(a)gmail.com>
     Copyright (C) 2013 Mattias <mattiasmun(a)gmail.com>
+    Copyright (C) 2016 Andrea Stacchiotti <andreastacchiotti(a)gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,7 +20,6 @@
 """
 
 import sys
-import codecs
 
 from gi.repository import Gtk
 
@@ -200,13 +200,6 @@ def menu_append_item(menu, name, callback, data=None):
     menu.append(item)
     item.connect('activate', callback, data)
 
-# for Python 3: convert to unicode just like Python 2 does
-def u(x):
-    if PY3K:
-        return codecs.unicode_escape_decode(x)[0]
-    else:
-        return x
-
 # Interface for bytes<>string conversion for py2/3
 # Usage is the same you'd do in py3, call `decode` on external raw data
 # and `encode` to work with the memory representation
@@ -221,3 +214,10 @@ def encode(unicode_string, errors='strict'):
         return unicode_string.encode(errors=errors)
     else:
         return unicode_string
+
+# Convert codepoints to integers byte by byte
+def str2bytes(string):
+    if PY3K:
+        return bytes(string)
+    else:
+        return map(ord, string)