scanmem/scanmem

commit 2b46e435896831b4b37fd012915d5c0b71b02c6b

scanroutines: Remove rounding from FLOAT_EQUALTO
Currently, the FLOAT_EQUALTO routines round both values to integers.
This makes exact matching impossible. With the ranges feature, the
user is free to use any rounding or range he likes and can also
emulate the current behavior.
So remove the rounding and do exact matching instead.

Fixes: GitHub issue #156
Reported-by: Jude Taylor (@pikajude)

scanroutines.c
@@ -132,13 +132,12 @@ DEFINE_FLOAT_ROUTINE_FOR_ALL_FLOAT_TYPE(LESSTHAN, <, user_value)
 /*-----------------------------------*/
 /* special EQUALTO for float numbers */
 /*-----------------------------------*/
-/* currently we round both of them to integers and compare them */
-/* TODO: let user specify a float number */
 #define DEFINE_FLOAT_EQUALTO_ROUTINE(FLOATTYPENAME, WIDTH) \
     int scan_routine_##FLOATTYPENAME##_EQUALTO SCAN_ROUTINE_ARGUMENTS \
     { \
         int ret = 0; \
-        if((new_value->flags.f##WIDTH##b) && (user_value->flags.f##WIDTH##b) && ((int##WIDTH##_t)get_f##WIDTH##b(new_value) == (int##WIDTH##_t)get_f##WIDTH##b(user_value))) \
+        if ((new_value->flags.f##WIDTH##b) && (user_value->flags.f##WIDTH##b) \
+            && (get_f##WIDTH##b(new_value) == get_f##WIDTH##b(user_value))) \
         { \
             SET_FLAG(saveflags, f##WIDTH##b); \
             ret = (WIDTH)/8; \

commit 6f788c07161dbc12547b49aa2e664e8c4faed86c

scanroutines: Declare scan_function_FLOATNN_EQUALTO() with standard macro
Commit 2b46e4358 removed the rounding special-case, so now just
declare it from the standard macro, to reduce clutter.

Fixes: 2b46e4358

scanroutines.c
@@ -118,7 +118,8 @@ DEFINE_FLOAT_MATCHANY_ROUTINE(FLOAT64, 64)
     DEFINE_FLOAT_ROUTINE(FLOAT32, 32, MATCHTYPENAME, MATCHTYPE, VALUE_TO_COMPARE_WITH) \
     DEFINE_FLOAT_ROUTINE(FLOAT64, 64, MATCHTYPENAME, MATCHTYPE, VALUE_TO_COMPARE_WITH) 
 
-DEFINE_FLOAT_ROUTINE_FOR_ALL_FLOAT_TYPE(NOTEQUALTO, !=, user_value)   /* TODO: this should be consistent with !EQUALTO */
+DEFINE_FLOAT_ROUTINE_FOR_ALL_FLOAT_TYPE(EQUALTO, ==, user_value)
+DEFINE_FLOAT_ROUTINE_FOR_ALL_FLOAT_TYPE(NOTEQUALTO, !=, user_value)
 DEFINE_FLOAT_ROUTINE_FOR_ALL_FLOAT_TYPE(NOTCHANGED, ==, old_value)         /* this is bad */
 DEFINE_FLOAT_ROUTINE_FOR_ALL_FLOAT_TYPE(CHANGED, !=, old_value)      /* this is bad, but better than above */
 DEFINE_FLOAT_ROUTINE_FOR_ALL_FLOAT_TYPE(INCREASED, >, old_value)
@@ -129,25 +130,6 @@ DEFINE_FLOAT_ROUTINE_FOR_ALL_FLOAT_TYPE(LESSTHAN, <, user_value)
 /********************/
 /* Special routines */
 /********************/
-
-/*-----------------------------------*/
-/* special EQUALTO for float numbers */
-/*-----------------------------------*/
-#define DEFINE_FLOAT_EQUALTO_ROUTINE(FLOATTYPENAME, WIDTH) \
-    extern inline int scan_routine_##FLOATTYPENAME##_EQUALTO SCAN_ROUTINE_ARGUMENTS \
-    { \
-        int ret = 0; \
-        if ((new_value->flags.f##WIDTH##b) && (user_value->flags.f##WIDTH##b) \
-            && (get_f##WIDTH##b(new_value) == get_f##WIDTH##b(user_value))) \
-        { \
-            SET_FLAG(saveflags, f##WIDTH##b); \
-            ret = (WIDTH)/8; \
-        } \
-        return ret; \
-    }
-
-DEFINE_FLOAT_EQUALTO_ROUTINE(FLOAT32, 32)
-DEFINE_FLOAT_EQUALTO_ROUTINE(FLOAT64, 64)
 
 /*---------------------------------*/
 /* for INCREASEDBY and DECREASEDBY */