scanmem/scanmem

commit 7d2c52c6b9bd12717f122b3045162e1f9f97a9dd

maps, handlers: Replace `alloca` with VLA/malloc

handlers.c
@@ -36,7 +36,6 @@
 #include <signal.h>
 #include <assert.h>
 #include <setjmp.h>
-#include <alloca.h>
 #include <strings.h>
 #include <string.h>
 #include <stdbool.h>
@@ -69,8 +68,6 @@
  * Commands are allowed to read and modify settings in the vars structure.
  *
  */
-
-#define calloca(x,y) (memset(alloca((x) * (y)), 0x00, (x) * (y)))
 
 /* try to determine the size of a pointer */
 #ifndef ULONG_MAX
@@ -122,7 +119,7 @@ bool handler__set(globals_t * vars, char **argv, unsigned argc)
 
     /* --- parse arguments into settings structs --- */
 
-    settings = calloca(argc - 1, sizeof(struct setting));
+    settings = calloc(argc - 1, sizeof(struct setting));
 
     /* parse every block into a settings struct */
     for (block = 0; block < argc - 1; block++) {
@@ -182,8 +179,7 @@ bool handler__set(globals_t * vars, char **argv, unsigned argc)
     if (INTERRUPTABLE()) {
         
         /* control returns here when interrupted */
-// settings is allocated with alloca, do not free it
-//        free(settings);
+        free(settings);
         sm_detach(vars->target);
         ENDINTERRUPTABLE();
         return true;
@@ -1144,7 +1140,7 @@ bool handler__shell(globals_t * vars, char **argv, unsigned argc)
         len += strlen(argv[i]);
 
     /* allocate space */
-    command = calloca(len, 1);
+    command = calloc(len, 1);
 
     /* concatenate strings */
     for (i = 1; i < argc; i++) {
@@ -1154,14 +1150,12 @@ bool handler__shell(globals_t * vars, char **argv, unsigned argc)
 
     /* finally execute command */
     if (system(command) == -1) {
-// command is allocated with alloca, do not free it
-//        free(command);
+        free(command);
         show_error("system() failed, command was not executed.\n");
         return false;
     }
 
-// command is allocated with alloca, do not free it
-//    free(command);
+    free(command);
     return true;
 }
 

maps.c
@@ -33,7 +33,6 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-#include <alloca.h>
 #include <stdbool.h>
 #include <unistd.h>
 
@@ -91,16 +90,12 @@ bool sm_readmaps(pid_t target, list_t *regions)
         while (getline(&line, &len, maps) != -1) {
             unsigned long start, end;
             region_t *map = NULL;
-            char read, write, exec, cow, *filename;
+            char read, write, exec, cow;
             int offset, dev_major, dev_minor, inode;
             region_type_t type = REGION_TYPE_MISC;
 
             /* slight overallocation */
-            if ((filename = alloca(len)) == NULL) {
-                show_error("failed to allocate %lu bytes for filename.\n", (unsigned long)len);
-                goto error;
-            }
-            
+            char filename[len];
             /* initialise to zero */
             memset(filename, '\0', len);
 

scanmem.h
@@ -42,7 +42,8 @@
 
 /* from string.h in glibc for Android/BSD */
 #ifndef strdupa
-#define strdupa(s)                                                            \
+# include <alloca.h>
+# define strdupa(s)                                                           \
     ({                                                                        \
       const char *__old = (s);                                                \
       size_t __len = strlen(__old) + 1;                                       \
@@ -52,7 +53,8 @@
 #endif
 
 #ifndef strndupa
-#define strndupa(s, n)                                                        \
+# include <alloca.h>
+# define strndupa(s, n)                                                       \
     ({                                                                        \
       const char *__old = (s);                                                \
       size_t __len = strnlen(__old, (n));                                     \

commit 78979e091a1c534b10edd07a2cd6fe3d625813a8

handlers: set(): free() settings array in every case
Found by Coverity Scan.

Fixes: 7d2c52c

handlers.c
@@ -313,10 +313,12 @@ bool handler__set(globals_t * vars, char **argv, unsigned argc)
         seconds++;
     }                           /* while(true) */
 
+    free(settings);
     ENDINTERRUPTABLE();
     return true;
 
 fail:
+    free(settings);
     ENDINTERRUPTABLE();
     return false;
     