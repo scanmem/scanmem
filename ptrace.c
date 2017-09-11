scanmem/scanmem

commit a03c3635dae9ef370adb3992ff28ff875f3e462e

scanroutines: Implement pow2 version of fast scan for small strings
Roll optimized version string-comparison scan_functions for
length power of 2, done by comparing them as uints.

Add an argument to sm_{get,choose}_scanroutine() to get
the string length in the choice process.

ptrace.c
@@ -288,7 +288,7 @@ bool sm_checkmatches(globals_t *vars,
     size_t bytes_at_next_sample;
     size_t bytes_per_sample;
 
-    if (sm_choose_scanroutine(vars->options.scan_data_type, match_type) == false)
+    if (sm_choose_scanroutine(vars->options.scan_data_type, match_type, &uservalue->flags) == false)
     {
         show_error("unsupported scan for current data type.\n");
         return false;
@@ -463,7 +463,7 @@ bool sm_searchregions(globals_t *vars, scan_match_type_t match_type, const userv
     match_flags zero_flag;
     zero_match_flags(&zero_flag);
     
-    if (sm_choose_scanroutine(vars->options.scan_data_type, match_type) == false)
+    if (sm_choose_scanroutine(vars->options.scan_data_type, match_type, &uservalue->flags) == false)
     {
         show_error("unsupported scan for current data type.\n"); 
         return false;

scanroutines.c
@@ -332,6 +332,32 @@ extern inline int scan_routine_STRING_EQUALTO SCAN_ROUTINE_ARGUMENTS
 
     return length;
 }
+
+/* optimized routines for small strings
+   careful: WIDTH = 8*LENGTH */
+
+#define DEFINE_STRING_POW2_EQUALTO_ROUTINE(WIDTH) \
+    extern inline int scan_routine_STRING##WIDTH##_EQUALTO SCAN_ROUTINE_ARGUMENTS \
+    { \
+        if (new_value->flags.length && \
+            (get_u##WIDTH##b(new_value) == *(uint##WIDTH##_t*)(user_value->string_value))) \
+        { \
+            /* matched */ \
+            saveflags->length = (WIDTH)/8; \
+            return (WIDTH)/8; \
+        } \
+        else \
+        { \
+            /* not matched */ \
+            return 0; \
+        } \
+    }
+
+DEFINE_STRING_POW2_EQUALTO_ROUTINE(8)
+DEFINE_STRING_POW2_EQUALTO_ROUTINE(16)
+DEFINE_STRING_POW2_EQUALTO_ROUTINE(32)
+DEFINE_STRING_POW2_EQUALTO_ROUTINE(64)
+
 /*-------------------------*/
 /* Any-xxx types specifiec */
 /*-------------------------*/
@@ -397,14 +423,39 @@ DEFINE_ANYTYPE_ROUTINE(RANGE)
     CHOOSE_ROUTINE(ANYFLOAT, ANYFLOAT, SCANMATCHTYPE, ROUTINEMATCHTYPENAME) \
     CHOOSE_ROUTINE(ANYNUMBER, ANYNUMBER, SCANMATCHTYPE, ROUTINEMATCHTYPENAME) \
 
+#define CHOOSE_ROUTINE_STRING(SCANMATCHTYPE, ROUTINEMATCHTYPENAME, LENGTH) \
+    if (mt == SCANMATCHTYPE) \
+    { \
+        switch (LENGTH) \
+        { \
+            case 0: \
+                assert(false); \
+                break; \
+            case 1: \
+                return &scan_routine_STRING8_##ROUTINEMATCHTYPENAME; \
+                break; \
+            case 2: \
+                return &scan_routine_STRING16_##ROUTINEMATCHTYPENAME; \
+                break; \
+            case 4: \
+                return &scan_routine_STRING32_##ROUTINEMATCHTYPENAME; \
+                break; \
+            case 8: \
+                return &scan_routine_STRING64_##ROUTINEMATCHTYPENAME; \
+                break; \
+            default: \
+                return &scan_routine_STRING_##ROUTINEMATCHTYPENAME; \
+                break; \
+        } \
+    }
 
 
-bool sm_choose_scanroutine(scan_data_type_t dt, scan_match_type_t mt)
+bool sm_choose_scanroutine(scan_data_type_t dt, scan_match_type_t mt, const match_flags* uflags)
 {
-    return (sm_scan_routine = sm_get_scanroutine(dt, mt)) != NULL;
+    return (sm_scan_routine = sm_get_scanroutine(dt, mt, uflags)) != NULL;
 }
 
-scan_routine_t sm_get_scanroutine(scan_data_type_t dt, scan_match_type_t mt)
+scan_routine_t sm_get_scanroutine(scan_data_type_t dt, scan_match_type_t mt, const match_flags* uflags)
 {
     CHOOSE_ROUTINE_FOR_ALL_NUMBER_TYPES(MATCHANY, ANY)
     CHOOSE_ROUTINE_FOR_ALL_NUMBER_TYPES(MATCHEQUALTO, EQUALTO)
@@ -419,8 +470,11 @@ scan_routine_t sm_get_scanroutine(scan_data_type_t dt, scan_match_type_t mt)
     CHOOSE_ROUTINE_FOR_ALL_NUMBER_TYPES(MATCHLESSTHAN, LESSTHAN)
     CHOOSE_ROUTINE_FOR_ALL_NUMBER_TYPES(MATCHRANGE, RANGE)
 
-    CHOOSE_ROUTINE(BYTEARRAY, BYTEARRAY, MATCHEQUALTO, EQUALTO) 
-    CHOOSE_ROUTINE(STRING, STRING, MATCHEQUALTO, EQUALTO) 
+    CHOOSE_ROUTINE(BYTEARRAY, BYTEARRAY, MATCHEQUALTO, EQUALTO)
+
+    if (uflags && dt == STRING) {
+        CHOOSE_ROUTINE_STRING(MATCHEQUALTO, EQUALTO, uflags->length)
+    }
 
     return NULL;
 }

scanroutines.h
@@ -71,8 +71,8 @@ extern scan_routine_t sm_scan_routine;
  * Choose the global scanroutine according to the given parameters, sm_scan_routine will be set.
  * Returns whether a proper routine has been found.
  */
-bool sm_choose_scanroutine(scan_data_type_t dt, scan_match_type_t mt);
+bool sm_choose_scanroutine(scan_data_type_t dt, scan_match_type_t mt, const match_flags* uflags);
 
-scan_routine_t sm_get_scanroutine(scan_data_type_t dt, scan_match_type_t mt);
+scan_routine_t sm_get_scanroutine(scan_data_type_t dt, scan_match_type_t mt, const match_flags* uflags);
 
 #endif /* SCANROUTINES_H */

handlers.c
@@ -1260,7 +1260,7 @@ bool handler__watch(globals_t * vars, char **argv, unsigned argc)
         /* check if the new value is different */
         match_flags tmpflags;
         zero_match_flags(&tmpflags);
-        scan_routine_t valuecmp_routine = (sm_get_scanroutine(ANYNUMBER, MATCHCHANGED));
+        scan_routine_t valuecmp_routine = (sm_get_scanroutine(ANYNUMBER, MATCHCHANGED, NULL));
         if (valuecmp_routine(&o, &n, NULL, &tmpflags, address)) {
 
             valcpy(&o, &n);

commit 3dddf4efc0c6f9f481070e6489f476545aadb9fe

libscanmem: Allow a NULL uservalue in `sm_choose_scanroutine()`
Change the signature of `sm_choose_scanroutine()` to accept an
uservalue and then check if it's NULL before dereferncing it to
give the flags to `sm_get_scanroutine()`.

Fixes possible NULL dereferences in snapshot and update.

Fixes: a03c3635d

ptrace.c
@@ -288,7 +288,7 @@ bool sm_checkmatches(globals_t *vars,
     size_t bytes_at_next_sample;
     size_t bytes_per_sample;
 
-    if (sm_choose_scanroutine(vars->options.scan_data_type, match_type, &uservalue->flags) == false)
+    if (sm_choose_scanroutine(vars->options.scan_data_type, match_type, uservalue) == false)
     {
         show_error("unsupported scan for current data type.\n");
         return false;
@@ -463,7 +463,7 @@ bool sm_searchregions(globals_t *vars, scan_match_type_t match_type, const userv
     match_flags zero_flag;
     zero_match_flags(&zero_flag);
     
-    if (sm_choose_scanroutine(vars->options.scan_data_type, match_type, &uservalue->flags) == false)
+    if (sm_choose_scanroutine(vars->options.scan_data_type, match_type, uservalue) == false)
     {
         show_error("unsupported scan for current data type.\n"); 
         return false;

scanroutines.c
@@ -583,7 +583,9 @@ scan_routine_t sm_get_scanroutine(scan_data_type_t dt, scan_match_type_t mt, con
 }
 
 
-bool sm_choose_scanroutine(scan_data_type_t dt, scan_match_type_t mt, const match_flags* uflags)
+bool sm_choose_scanroutine(scan_data_type_t dt, scan_match_type_t mt, const uservalue_t* uval)
 {
-    return (sm_scan_routine = sm_get_scanroutine(dt, mt, uflags)) != NULL;
+    const match_flags *uflags = uval ? &(uval->flags) : NULL;
+    sm_scan_routine = sm_get_scanroutine(dt, mt, uflags);
+    return (sm_scan_routine != NULL);
 }

scanroutines.h
@@ -71,7 +71,7 @@ extern scan_routine_t sm_scan_routine;
  * Choose the global scanroutine according to the given parameters, sm_scan_routine will be set.
  * Returns whether a proper routine has been found.
  */
-bool sm_choose_scanroutine(scan_data_type_t dt, scan_match_type_t mt, const match_flags* uflags);
+bool sm_choose_scanroutine(scan_data_type_t dt, scan_match_type_t mt, const uservalue_t* uval);
 
 scan_routine_t sm_get_scanroutine(scan_data_type_t dt, scan_match_type_t mt, const match_flags* uflags);
 