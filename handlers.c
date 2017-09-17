scanmem/scanmem

commit cae89d30569ecd208f781aea3e474f4807e05c94

handlers: Add a `max_to_print` parameter to `list`
Add an optional parameter to limit the amount of matches printed.
Defaults to 10k.

Also clean up a bit the whole `handler__list()` and update
the documentation.

handlers.c
@@ -326,13 +326,13 @@ fail:
     
 }
 
-/* XXX: add yesno command to check if matches > 099999 */
-/* FORMAT (don't change, front-end depends on this): 
+/* Accepts a numerical argument to print up to N matches, defaults to 10k
+ * FORMAT (don't change, front-end depends on this):
  * [#no] addr, value, [possible types (separated by space)]
  */
 bool handler__list(globals_t *vars, char **argv, unsigned argc)
 {
-    unsigned i = 0;
+    unsigned long num = 0;
     size_t buf_len = 128; /* will be realloc'd later if necessary */
     element_t *np = NULL;
     char *v = malloc(buf_len);
@@ -341,10 +341,18 @@ bool handler__list(globals_t *vars, char **argv, unsigned argc)
         show_error("memory allocation failed.\n");
         return false;
     }
-    char *bytearray_suffix = ", [bytearray]";
-    char *string_suffix = ", [string]";
+    const char *bytearray_suffix = ", [bytearray]";
+    const char *string_suffix = ", [string]";
 
-    USEPARAMS();
+    unsigned long max_to_print = 10000;
+    if (argc > 1) {
+        max_to_print = strtoul(argv[1], NULL, 0x00);
+
+        if (max_to_print == 0) {
+            show_error("`%s` is not a valid positive integer.\n", argv[1]);
+            return false;
+        }
+    }
 
     if (!vars->matches)
         goto out_free;
@@ -357,6 +365,10 @@ bool handler__list(globals_t *vars, char **argv, unsigned argc)
 
     /* list all known matches */
     while (reading_swath_index->first_byte_in_child) {
+        if (num == max_to_print) {
+            show_user("[...]\n");
+            break;
+        }
 
         match_flags flags = reading_swath_index->data[reading_iterator].match_info;
 
@@ -366,7 +378,6 @@ bool handler__list(globals_t *vars, char **argv, unsigned argc)
             switch(vars->options.scan_data_type)
             {
             case BYTEARRAY:
-                ; /* cheat gcc */ 
                 buf_len = flags.length * 3 + 32;
                 v = realloc(v, buf_len); /* for each byte and the suffix, this should be enough */
 
@@ -380,7 +391,6 @@ bool handler__list(globals_t *vars, char **argv, unsigned argc)
                 strcat(v, bytearray_suffix);
                 break;
             case STRING:
-                ; /* cheat gcc */
                 buf_len = flags.length + strlen(string_suffix) + 32; /* for the string and suffix, this should be enough */
                 v = realloc(v, buf_len);
                 if (v == NULL)
@@ -421,8 +431,8 @@ bool handler__list(globals_t *vars, char **argv, unsigned argc)
                 }
                 np = np->next;
             }
-            fprintf(stdout, "[%2u] "POINTER_FMT", %2u + "POINTER_FMT", %5s, %s\n", 
-                    i++, address_ul, region_id, match_off, region_type, v);
+            printf("[%2lu] "POINTER_FMT", %2u + "POINTER_FMT", %5s, %s\n",
+                   num++, address_ul, region_id, match_off, region_type, v);
         }
 
         /* go on to the next one... */

handlers.h
@@ -65,12 +65,14 @@
 
 bool handler__set(globals_t *vars, char **argv, unsigned argc);
 
-#define LIST_SHRTDOC "list all currently known matches"
-#define LIST_LONGDOC "usage: list\n" \
-               "Print all the currently known matches, along with details about the\n" \
+#define LIST_SHRTDOC "list currently known matches"
+#define LIST_LONGDOC "usage: list [max_to_print]\n" \
+               "Print currently known matches, along with details about the\n" \
                "match, such as its type, location, and last known value. The number in\n" \
                "the left column is the `match-id`, this can be passed to other commands\n" \
                "such as `set`, `delete`, etc.\n" \
+               "By default `list` prints up to 10k matches, a numerical parameter" \
+               "can be given to change this limit.\n" \
                "The flags displayed indicate the possible types of the variable.\n" \
                "Also the region id, an offset and the region type belonging to a match\n" \
                "are displayed. The offset is used from the code load address or region start.\n" \

commit 79f45f026bbf87ab8d795ebe8a4c72135828f6d3

handlers: list(): Fix a memory leak in case of invalid max_to_print
Allocate the array after checking for input, so that freeing it
it's not needed.

Also fix printing of `[...]` in an edge case.

Fixes: cae89d3

handlers.c
@@ -336,12 +336,7 @@ bool handler__list(globals_t *vars, char **argv, unsigned argc)
     unsigned long num = 0;
     size_t buf_len = 128; /* will be realloc'd later if necessary */
     element_t *np = NULL;
-    char *v = malloc(buf_len);
-    if (v == NULL)
-    {
-        show_error("memory allocation failed.\n");
-        return false;
-    }
+    char *v;
     const char *bytearray_suffix = ", [bytearray]";
     const char *string_suffix = ", [string]";
 
@@ -356,7 +351,13 @@ bool handler__list(globals_t *vars, char **argv, unsigned argc)
     }
 
     if (!vars->matches)
-        goto out_free;
+        return false;
+
+    if ((v = malloc(buf_len)) == NULL)
+    {
+        show_error("memory allocation failed.\n");
+        return false;
+    }
 
     if (vars->regions)
         np = vars->regions->head;
@@ -367,7 +368,8 @@ bool handler__list(globals_t *vars, char **argv, unsigned argc)
     /* list all known matches */
     while (reading_swath_index->first_byte_in_child) {
         if (num == max_to_print) {
-            show_user("[...]\n");
+            if (num < vars->num_matches)
+                show_user("[...]\n");
             break;
         }
 
@@ -445,7 +447,6 @@ bool handler__list(globals_t *vars, char **argv, unsigned argc)
         }
     }
 
-out_free:
     free(v);
     return true;
 }