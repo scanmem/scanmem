scanmem/scanmem

commit a3134f111fe0667d9d0c709a30a614178b42d876

libscanmem: add sets.[ch], implement w/ delete
sets.[ch]:
- New files add centralized user set parsing.

handlers.[ch]:
* Plug-in the new set parser.
* Updated the `delete` documentation to reflect the changes introduced
  by this commit.

sets.h
@@ -1,0 +1,53 @@
+/*
+    Create sets.
+
+    Copyright (C) 2016-2017 Bijan Kazemi <bkazemi@users.sf.net>
+
+    This file is part of libscanmem.
+
+    This library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SETS_H
+#define SETS_H
+
+#include <stdlib.h>
+
+struct set {
+    size_t  size; /* size of set (used) */
+    size_t *buf;  /* value buffer       */
+};
+
+inline void set_cleanup(struct set *set)
+{
+    if (set)
+        free(set->buf);
+}
+
+/* iterate over set from front (forwards) */
+#define foreach_set_fw(i, set) \
+    for (size_t i = 0; i < (set)->size; i++)
+
+/*
+ * iterate over set from back (backwards)
+ *
+ * NOTE: `_reserved_for_set_iteration_zzaw2_df_` is reserved, named randomly to avoid namespace crash
+ */
+#define foreach_set_bw(i, set)                                                \
+    for (size_t _reserved_for_set_iteration_zzaw2_df_ = 0, i = (set)->size-1; \
+         _reserved_for_set_iteration_zzaw2_df_ < (set)->size;                 \
+         _reserved_for_set_iteration_zzaw2_df_++, i--)
+
+bool parse_uintset(const char *, struct set *, size_t);
+#endif /* SETS_H */

Makefile.am
@@ -15,6 +15,7 @@ libscanmem_la_include_HEADERS = commands.h \
     maps.h \
     scanmem.h \
     scanroutines.h \
+    sets.h \
     show_message.h \
     targetmem.h \
     value.h
@@ -30,6 +31,7 @@ libscanmem_la_SOURCES = commands.c \
     maps.c \
     scanmem.c \
     scanroutines.c \
+    sets.c \
     show_message.c \
     targetmem.c \
     value.c 

handlers.c
@@ -52,6 +52,7 @@
 #include "endianness.h"
 #include "handlers.h"
 #include "interrupt.h"
+#include "sets.h"
 #include "show_message.h"
 
 #define USEPARAMS() ((void) vars, (void) argv, (void) argc)     /* macro to hide gcc unused warnings */
@@ -451,44 +452,45 @@ bool handler__list(globals_t *vars, char **argv, unsigned argc)
     return true;
 }
 
-/* XXX: handle multiple deletes, eg delete !1 2 3 4 5 6 */
 bool handler__delete(globals_t * vars, char **argv, unsigned argc)
 {
     size_t id;
-    char *end = NULL;
     match_location loc;
+    struct set del_set;
 
     if (argc != 2) {
         show_error("was expecting one argument, see `help delete`.\n");
         return false;
     }
 
-    /* parse argument */
-    id = strtoul(argv[1], &end, 0x00);
+    if (vars->num_matches == 0) {
+        show_error("nothing to delete.\n");
+        return false;
+    }
 
-    /* check that strtoul() worked */
-    if (argv[1][0] == '\0' || *end != '\0') {
-        show_error("sorry, couldnt parse `%s`, try `help delete`\n", argv[1]);
+    if (!parse_uintset(argv[1], &del_set, (size_t)vars->num_matches)) {
+        show_error("failed to parse the set, try `help delete`.\n");
         return false;
     }
-    
-    loc = nth_match(vars->matches, id);
-    
-    if (loc.swath)
-    {
-        /* it is not convenient to check whether anything else relies on this,
-           so just mark it as not a REAL match */
-        zero_match_flags(&loc.swath->data[loc.index].match_info);
-        vars->num_matches--;
-        return true;
+
+    foreach_set_bw(i, &del_set) {
+        id  = del_set.buf[i];
+        loc = nth_match(vars->matches, id);
+
+        if (loc.swath) {
+            /* It is not convenient to check whether anything else relies on this,
+               so just mark it as not a REAL match */
+            zero_match_flags(&loc.swath->data[loc.index].match_info);
+            vars->num_matches--;
+        } else {
+            show_error("BUG: delete: id <%zu> match failure\n", id);
+            set_cleanup(&del_set);
+            return false;
+        }
     }
-    else
-    {
-        /* I guess this is not a valid match-id */
-        show_warn("you specified a non-existant match `%u`.\n", id);
-        show_info("use \"list\" to list matches, or \"help\" for other commands.\n");
-        return false;
-    }
+    set_cleanup(&del_set);
+
+    return true;
 }
 
 bool handler__reset(globals_t * vars, char **argv, unsigned argc)

handlers.h
@@ -80,16 +80,24 @@ bool handler__set(globals_t *vars, char **argv, unsigned argc);
 
 bool handler__list(globals_t *vars, char **argv, unsigned argc);
 
-#define DELETE_SHRTDOC "delete a known match by match-id"
-#define DELETE_LONGDOC "usage: delete match-id\n" \
-                "Remove the match `match-id` from the match list. The `match-id` can\n" \
+#define DELETE_SHRTDOC "delete known matches by match-id"
+#define DELETE_LONGDOC "usage: delete <match-id set>\n" \
+                "Remove a set of match-id's from the match list. The set format\n" \
+                "is described below. The list of match-id's can\n" \
                 "be found using the `list` command. To delete all matches, see\n" \
                 "the `reset` command.\n" \
                 "To delete all matches associated with a particular library, see the\n" \
-                "`dregion` command, which will also remove any associated matches.\n" \
-                "Example:\n" \
-                "\tdelete 0 - delete match 0\n" \
-                "NOTE: match-ids may be recalculated after matches are removed or added."
+                "`dregion` command, which will also remove any associated matches.\n\n" \
+                "The Set Format:\n" \
+                "[..a](,b..c | d, ...)[e..]\n" \
+                "The beginning and end of the set may include an optional \"open-ended\" range -\n" \
+                "`..a` means a range starting at match-id `0` to match-id `a`. `e..` means a range starting at\n" \
+                "match-id `e` to the last match-id. The rest is the same as the standard format.\n\n" \
+                "Examples:\n" \
+                "\tdelete ..5,10..15 - delete matches 0 through 5 and 10 through 15\n" \
+                "\tdelete ..5,14,20.. - delete matches 0 through 5, 14, and 20 through the last match\n\n" \
+                "NOTE: Match-ids may be recalculated after matches are removed or added. However, the set\n" \
+                "      of matches is guaranteed to be deleted properly."
 
 bool handler__delete(globals_t *vars, char **argv, unsigned argc);
 

sets.c
@@ -1,0 +1,310 @@
+/*
+    Create sets.
+
+    Copyright (C) 2016-2017 Bijan Kazemi <bkazemi@users.sf.net>
+
+    This file is part of libscanmem.
+
+    This library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <assert.h>
+#include <errno.h>
+
+#include "show_message.h"
+#include "sets.h"
+
+#define DEFAULT_UINTLS_SZ (64)
+
+/*
+ * -= The Set Data-Type =-
+ *
+ * A 'set' is a type similar in vein to the mathematical definition of a set.
+ * See: https://foldoc.org/set
+ *
+ * Sets support ranges. See the `delete` longdoc for more information.
+ *
+ * Restrictions of our set data-type:
+ * - No empty sets.
+ * - No duplicate elements.
+ * - All the elements must be of the same type.
+ */
+
+/*
+ * Used with `qsort()`.
+ *
+ * This function uses the less elegant branching solution
+ * to deal with possible overflow.
+ */
+static int _size_t_cmp(const void *_i1, const void *_i2)
+{
+    const size_t i1 = *(const size_t *)_i1, i2 = *(const size_t *)_i2;
+
+    if      (i1 <  i2) return -1;
+    else if (i1 == i2) return  0;
+    else               return  1;
+}
+
+/*
+ * Function used to grow the set array.
+ *
+ * We must use a separate pointer with realloc(), in case it fails. When that
+ * happens and we re-assign valarr, valarr is _not_ free()'d by realloc(), and
+ * valarr is then assigned to NULL, leaving the original valarr address lost.
+ */
+static inline bool inc_arr_sz(size_t **valarr, size_t *arr_maxsz, size_t maxsz)
+{
+    size_t *valarr_tmpptr;
+
+    if (*arr_maxsz > maxsz / 2)
+        *arr_maxsz = maxsz;
+    else
+        *arr_maxsz *= 2;
+
+    if ((valarr_tmpptr = realloc(valarr, *arr_maxsz * sizeof(size_t))) == NULL)
+        return false;
+
+    *valarr = valarr_tmpptr;
+
+    return true;
+}
+
+bool parse_uintset(const char *lptr, struct set *set, size_t maxsz)
+{
+    const char        *tok, *tmpnum = NULL, *tmpnum_end;
+    char              *tmpnum_endptr = NULL, *fail_reason = "BUG";
+    bool               got_num, is_hex = false;
+    size_t             last_num = 0, arr_szfilled, arr_maxsz;
+    size_t            *valarr;
+    size_t             vaidx = 0;
+    unsigned long long toknum;
+
+    assert(lptr && set);
+
+    /* ensure set is initialized */
+    memset(set, 0, sizeof(*set));
+
+    enum {
+        NIL = -1,
+        NUMBER_TOK,
+        RANGE_TOK,
+        COMMA_TOK,
+    } last_type = NIL;
+
+    /* allocate space for value pointers */
+    if ((valarr = malloc(DEFAULT_UINTLS_SZ * sizeof(size_t))) == NULL) {
+        show_error("%s(): OOM (Out Of Memory)\n", __func__);
+        return false;
+    }
+
+    arr_szfilled = 0;
+    arr_maxsz    = DEFAULT_UINTLS_SZ;
+    got_num      = false;
+
+    for (tok = lptr; *tok; tok++) {
+        /* skip spaces */
+        while (tok && isspace(*tok))
+            tok++;
+
+        /* check if we just went over trailing space */
+        if (!tok || !*tok)
+            break;
+
+        assert(arr_szfilled <= arr_maxsz); // should never fail
+        if (arr_szfilled == arr_maxsz)
+            if (!inc_arr_sz(&valarr, &arr_maxsz, maxsz)) {
+                fail_reason = "OOM (Out Of Memory)";
+                goto error;
+            }
+
+        switch (*tok) {
+        case ',':
+            if (last_type == RANGE_TOK) {
+                fail_reason = "invalid range";
+                goto error;
+            }
+            last_type = COMMA_TOK;
+            continue;
+        case '.':
+            if (last_type == COMMA_TOK || last_type == RANGE_TOK) {
+                fail_reason = "invalid range";
+                goto error;
+            }
+            /* check for consecutive `..` */
+            if (tok[1] != '.') {
+                fail_reason = "bad token";
+                goto error;
+            }
+            last_type = RANGE_TOK;
+            tok++;
+            continue;
+        }
+
+        if (isdigit(*tok)) {
+            /* check for 0x hex prefix */
+            if (*tok == '0' && tolower(tok[1]) == 'x') {
+                if (!isxdigit(tok[2]))
+                    continue;
+                is_hex = true;
+                tok   += 2;
+            }
+
+            /* find the end of this number in string */
+            tmpnum_end = tok;
+            for ( ; isxdigit(*tmpnum_end); tmpnum_end++)
+                ;
+            tmpnum = strndup(tok, tmpnum_end - tok);
+
+            errno  = 0;
+            toknum = strtoull(tmpnum, &tmpnum_endptr, is_hex ? 16 : 10);
+            if (is_hex)
+                is_hex = false;
+            if (errno || *tmpnum_endptr != '\0') {
+                fail_reason = "strtoull() failed";
+                goto error;
+            }
+
+            free((void *)tmpnum);
+            tmpnum = NULL;
+
+            if (last_type == RANGE_TOK) {
+                if (!got_num) {
+                    /* we've got a {0 .. n} range */
+                    last_num  = toknum;
+                    last_type = NUMBER_TOK;
+                    got_num   = true;
+                    /* move token position to last number found (needed for 2+ digit nums */
+                    tok = tmpnum_end-1;
+                    /* bounds check */
+                    if (last_num >= maxsz) {
+                        fail_reason = "0..n range OOB (Out Of Bounds)";
+                        goto error;
+                    }
+                    /* pre-set the new size filled and grow array if necessary */
+                    if ((arr_szfilled += last_num+1) > arr_maxsz) // +1 for `0`
+                        while (arr_szfilled > arr_maxsz) {
+                            if (!inc_arr_sz(&valarr, &arr_maxsz, maxsz)) {
+                                fail_reason = "OOM (Out Of Memory)";
+                                goto error;
+                            }
+                        }
+                    /* fill up array with range */
+                    for (size_t i = 0; i <= last_num; i++)
+                        valarr[vaidx++] = i;
+                    continue;
+                }
+                if (toknum <= last_num || toknum >= maxsz) {
+                    fail_reason = "invalid range";
+                    goto error;
+                }
+
+                /* pre-set the new size filled and grow array if necessary */
+                if ((arr_szfilled += (toknum - last_num)) > arr_maxsz)
+                    while (arr_szfilled > arr_maxsz) {
+                        if (!inc_arr_sz(&valarr, &arr_maxsz, maxsz)) {
+                            fail_reason = "OOM (Out Of Memory)";
+                            goto error;
+                        }
+                    }
+
+                /* fill up array with range */
+                for (size_t i = last_num+1; i <= toknum; i++)
+                    valarr[vaidx++] = i;
+            } else if (last_type == NUMBER_TOK) {
+                /* sanity check */
+                fail_reason = "impossible condition (last_type == NUMBER_TOK)";
+                goto error;
+            } else {
+                valarr[vaidx++] = toknum;
+                arr_szfilled++;
+            }
+            last_num  = toknum;
+            last_type = NUMBER_TOK;
+            if (!got_num)
+                got_num = true;
+            /* move token position to last number found (needed for 2+ digit nums) */
+            tok = tmpnum_end-1;
+        } else {
+            fail_reason = "bad token";
+            goto error;
+        }
+    }
+
+    /* check for {n .. end} range */
+    if (last_type == RANGE_TOK) {
+        if (!got_num) {
+            fail_reason = "invalid range";
+            goto error;
+        }
+        if (last_num >= maxsz) {
+            fail_reason = "n..end range OOB (Out Of Bounds)";
+            goto error;
+        }
+        if ((arr_szfilled += (maxsz-1 - last_num)) > arr_maxsz)
+            while (arr_szfilled > arr_maxsz) {
+                if (!inc_arr_sz(&valarr, &arr_maxsz, maxsz)) {
+                    fail_reason = "OOM (Out Of Memory)";
+                    goto error;
+                }
+            }
+        /* fill up array with range */
+        for (size_t i = last_num+1; i < maxsz; i++)
+            valarr[vaidx++] = i;
+    }
+
+    /* consider empty sets invalid */
+    if (arr_szfilled == 0) {
+        fail_reason = "empty set";
+        goto error;
+    }
+
+    /* sort the value array */
+    qsort(valarr, arr_szfilled, sizeof(valarr[0]), _size_t_cmp);
+
+    /* check if there are any duplicates */
+    for (size_t i = 0; i+1 < arr_szfilled; i++)
+        if (valarr[i] == valarr[i+1]) {
+            fail_reason = "duplicate element";
+            goto error;
+        }
+
+    /* check range (for individual entries, faster than checking each one) */
+    if (valarr[arr_szfilled-1] >= maxsz) {
+        fail_reason = "OOB (Out Of Bounds) element(s)";
+        goto error;
+    }
+
+    size_t *tmp_vaptr;
+    if ((tmp_vaptr = realloc(valarr, arr_szfilled * sizeof(size_t))) == NULL) {
+        fail_reason = "couldn't deallocate possibly unused end of the buffer";
+        goto error;
+    }
+    valarr = tmp_vaptr;
+
+    set->buf  = valarr;
+    set->size = arr_szfilled;
+    return true;
+
+error:
+    show_error("%s(): %s\n", __func__, fail_reason);
+    free((void *)tmpnum);
+    free(valarr);
+    set_cleanup(set);
+    return false;
+}

commit 53c2f616800627cf9a0ec9793cf440546370a753

sets: Declare `set_cleanup()` as `static inline`
Fixes the build in some edge cases, like `make CFLAGS=''`,
because of the different meaning of `inline` when used
in an header.

Fixes: a3134f1

sets.h
@@ -29,7 +29,7 @@ struct set {
     size_t *buf;  /* value buffer       */
 };
 
-inline void set_cleanup(struct set *set)
+static inline void set_cleanup(struct set *set)
 {
     if (set)
         free(set->buf);