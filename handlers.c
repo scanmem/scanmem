scanmem/scanmem

commit f8fbe26f9e9d8d967539ba01a8f511adbc672b4c

handlers: check for matches properly
Previously, we'd check if the `matches` struct is NULL, which may
be initialized when we have no matches.

handlers.c
@@ -351,7 +351,7 @@ bool handler__list(globals_t *vars, char **argv, unsigned argc)
         }
     }
 
-    if (!vars->matches)
+    if (vars->num_matches == 0)
         return false;
 
     if ((v = malloc(buf_len)) == NULL)
@@ -839,6 +839,10 @@ bool handler__operators(globals_t * vars, char **argv, unsigned argc)
     }
 
     if (vars->matches) {
+        if (vars->num_matches == 0) {
+            show_error("there are currently no matches.\n");
+            return false;
+        }
         if (sm_checkmatches(vars, m, &val) == false) {
             show_error("failed to search target address space.\n");
             return false;
@@ -1039,6 +1043,10 @@ bool handler__default(globals_t * vars, char **argv, unsigned argc)
 
     /* user has specified an exact value of the variable to find */
     if (vars->matches) {
+        if (vars->num_matches == 0) {
+            show_error("there are currently no matches.\n");
+            goto retl;
+        }
         /* already know some matches */
         if (sm_checkmatches(vars, m, val) != true) {
             show_error("failed to search target address space.\n");
@@ -1070,7 +1078,7 @@ bool handler__update(globals_t *vars, char **argv, unsigned argc)
 {
 
     USEPARAMS();
-    if (vars->matches) {
+    if (vars->num_matches) {
         if (sm_checkmatches(vars, MATCHANY, NULL) == false) {
             show_error("failed to scan target address space.\n");
             return false;

commit 333de8b468713d6c8b5c6dc5dc5f6e8124eab685

handlers: handler__string(): Check for previous matches properly
Add the proper check of f8fbe26 to `handler__string()` as well.

Fixes: f8fbe26

handlers.c
@@ -936,6 +936,10 @@ bool handler__string(globals_t * vars, char **argv, unsigned argc)
 
     /* user has specified an exact value of the variable to find */
     if (vars->matches) {
+        if (vars->num_matches == 0) {
+            show_error("there are currently no matches.\n");
+            return false;
+        }
         /* already know some matches */
         if (sm_checkmatches(vars, MATCHEQUALTO, &val) != true) {
             show_error("failed to search target address space.\n");