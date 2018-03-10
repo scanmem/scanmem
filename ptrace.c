scanmem/scanmem

commit 2bcbe8595c63055b7216f11d8b893e3353a207ed

ptrace: Micro-optimize the comparison expressions in `sm_peekdata()`
Replace the expressions used for the cache decision with
mathematically equivalent ones (as overflow is impossible in this case),
but that the compiler can optimize better.

On my system (gcc-7, amd64) this saves 10% time in a `snapshot;1` scan.

This optimization is really unstable, as equally simple and functionally
equivalent changes lead to slower code, so those ugly expressions
could maybe be rewritten better once compilers get smarter.

Also sneak in a trivial fix for a71e1520, as `readmemory()`
returns 0 on failure, not -1.

ptrace.c
@@ -234,7 +234,7 @@ extern inline bool sm_peekdata(const void *addr, uint16_t length, const mem64_t 
     /* check if we have a full cache hit */
     if (peekbuf.base != NULL &&
         reqaddr >= peekbuf.base &&
-        (unsigned long) (reqaddr + length - peekbuf.base) <= peekbuf.size)
+        peekbuf.base - reqaddr + peekbuf.size - length >= 0)
     {
         *result_ptr = (mem64_t*)&peekbuf.cache[reqaddr - peekbuf.base];
         *memlength = peekbuf.base - reqaddr + peekbuf.size;
@@ -242,7 +242,7 @@ extern inline bool sm_peekdata(const void *addr, uint16_t length, const mem64_t 
     }
     else if (peekbuf.base != NULL &&
              reqaddr >= peekbuf.base &&
-             (unsigned long) (reqaddr - peekbuf.base) < peekbuf.size)
+             peekbuf.base - reqaddr + peekbuf.size > 0)
     {
         assert(peekbuf.size != 0);
 
@@ -569,7 +569,7 @@ bool sm_searchregions(globals_t *vars, scan_match_type_t match_type, const userv
 
         /* read region into the `data` array */
         nread = readmemory(data, r->start, r->size);
-        if (nread == -1) {
+        if (nread == 0) {
             show_error("reading region %02u failed.\n", regnum);
             return false;
         }

commit c899ac335c479680ab2980dc62b9a73db699dcde

ptrace: Return to the old comparison expressions in `sm_peekdata()`
Commit 2bcbe85 "ptrace: Micro-optimize the comparison
expressions in `sm_peekdata()`" changed said expressions with
ones in which the pointer difference between `reqaddr` and
`peekbuf.base` is held in a temporary signed variable.

In 32 bit systems this variable can overflow (as pointer differences
can be bigger than 2^31), leading to crashes.
The old code performed the comparison in a way that's always safe.

Many (correct) variations of the expressions were tried, but
all of them regressed on speed in at least one of the two arches.
So just return to the old implementation.

Tested on ARM 32bit and amd64.

Fixes: 2bcbe859

ptrace.c
@@ -234,7 +234,7 @@ extern inline bool sm_peekdata(const void *addr, uint16_t length, const mem64_t 
     /* check if we have a full cache hit */
     if (peekbuf.base != NULL &&
         reqaddr >= peekbuf.base &&
-        peekbuf.base - reqaddr + peekbuf.size - length >= 0)
+        (unsigned long) (reqaddr + length - peekbuf.base) <= peekbuf.size)
     {
         *result_ptr = (mem64_t*)&peekbuf.cache[reqaddr - peekbuf.base];
         *memlength = peekbuf.base - reqaddr + peekbuf.size;
@@ -242,7 +242,7 @@ extern inline bool sm_peekdata(const void *addr, uint16_t length, const mem64_t 
     }
     else if (peekbuf.base != NULL &&
              reqaddr >= peekbuf.base &&
-             peekbuf.base - reqaddr + peekbuf.size > 0)
+             (unsigned long) (reqaddr - peekbuf.base) < peekbuf.size)
     {
         assert(peekbuf.size != 0);
 