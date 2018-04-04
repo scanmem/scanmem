d77b1713e548f6e26e62a3e146f852813bbee6d9
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2018-04-04 22:42:18 +0200

    targetmem.h: replace `uint` with `unsigned int`
    The header is part of the public API via `scanmem.h` and it was
    causing compilation issues for projects that
    did not explicitly typedef uint.
    
    Reported-by: @SSStormy
    Closes: #311

--- targetmem.h
+++ targetmem.h
@@ -257,7 +257,7 @@ static inline value_t
 data_to_val_aux (const matches_and_old_values_swath *swath,
                  size_t index, size_t swath_length)
 {
-    uint i;
+    unsigned int i;
     value_t val;
     size_t max_bytes = swath_length - index;
 
