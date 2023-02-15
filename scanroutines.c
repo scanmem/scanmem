c6045a8677f37a51b9760132b39fdfd265b1196a
Sam James <sam@gentoo.org>
2023-02-15 09:43:56 +0000

    Use 'unsigned int' instead of unofficial alias 'uint'
    glibc headers have 'uint' typedef'd to 'unsigned int', but this isn't
    guaranteed to be avaialble. Change to 'unsigned int' which is equivalent
    to fix building on musl.
    
    Bug: https://bugs.gentoo.org/854840

--- scanroutines.c
+++ scanroutines.c
@@ -369,7 +369,7 @@ extern inline unsigned int scan_routine_BYTEARRAY_EQUALTO SCAN_ROUTINE_ARGUMENTS
 {
     const uint8_t *bytes_array = user_value->bytearray_value;
     const wildcard_t *wildcards_array = user_value->wildcard_value;
-    uint length = user_value->flags;
+    unsigned int length = user_value->flags;
     if (memlength < length ||
         *((uint64_t*)bytes_array) != (memory_ptr->uint64_value & *((uint64_t*)wildcards_array)))
     {
@@ -470,7 +470,7 @@ DEFINE_BYTEARRAY_SMALLOOP_EQUALTO_ROUTINE(56)
 extern inline unsigned int scan_routine_STRING_EQUALTO SCAN_ROUTINE_ARGUMENTS
 {
     const char *scan_string = user_value->string_value;
-    uint length = user_value->flags;
+    unsigned int length = user_value->flags;
     if(memlength < length ||
        memory_ptr->int64_value != *((int64_t*)scan_string))
     {
